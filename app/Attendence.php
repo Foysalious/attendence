3e9ea89ffe07dd8ee9c970a6363e0709f58f9018
foysalious <foysalrahman112@gmail.com>
2022-07-16 20:48:24 +0600

    relation addded

--- app/Attendence.php
+++ app/Attendence.php
@@ -9,4 +9,9 @@ class Attendence extends Model
     protected $fillable = [
         'check_in_time', 'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function attendence()
+    {
+        return $this->hasMany(Attendence::class, 'user_id', 'id');
+    }
 }
